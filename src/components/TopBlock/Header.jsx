f042744a67ec056d0ef6cce210f6d777d92b0aa6
GQSM <ms314006@hotmail.com>
2021-06-06 13:14:20 +0800

    modify: 把 TopBlock 的 Header 單切成一個 Component

---
+++ src/components/TopBlock/Header.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import logoBlue from '../../assets/img/logo-blue.png';
+
+const Body = styled.div`
+  color: #1f3f83;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px 50px 25px 50px;
+  box-sizing: border-box;
+`;
+
+const LogoWrapper = styled.div`
+  & > img {
+    width: 132px;
+  }
+`;
+
+const Menu = styled.ul`
+  font-size: 24px;
+
+  & > li:nth-child(2) {
+    &::before {
+      content: "|";
+      margin: 0px 12px;
+    }
+  }
+`
+
+const MenuItem = styled.li`
+  float: left;
+
+  & > a {
+    text-decoration: none;
+
+    & :visited {
+      color: #1f3f83;
+    }
+  }
+`;
+
+const Header = () => (
+  <Body>
+    <LogoWrapper>
+      <img src={logoBlue} />
+    </LogoWrapper>
+    <Menu>
+      <MenuItem>
+        <a href="https://github.com/ms314006/biskett-clone">
+          OOOO
+        </a>
+      </MenuItem>
+      <MenuItem>
+        <a href="https://github.com/ms314006/biskett-clone">
+          XXXX
+        </a>
+      </MenuItem>
+    </Menu>
+  </Body>
+);
+
+export default Header;
--- src/components/TopBlock/TopBlock.jsx
+++ src/components/TopBlock/TopBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import logoBlue from '../../assets/img/logo-blue.png';
+import Header from './Header';
 import superQ1 from '../../assets/img/superQ1.png';
 
 const Body = styled.div`
@@ -11,44 +11,6 @@ const Body = styled.div`
   height: 100vh;
   background: #f0f7ff;
   overflow: hidden;
-`;
-
-const Header = styled.div`
-  color: #1f3f83;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 50px 50px 25px 50px;
-  box-sizing: border-box;
-`;
-
-const LogoWrapper = styled.div`
-  & > img {
-    width: 132px;
-  }
-`;
-
-const Menu = styled.ul`
-  font-size: 24px;
-
-  & > li:nth-child(2) {
-    &::before {
-      content: "|";
-      margin: 0px 12px;
-    }
-  }
-`
-
-const MenuItem = styled.li`
-  float: left;
-
-  & > a {
-    text-decoration: none;
-
-    & :visited {
-      color: #1f3f83;
-    }
-  }
 `;
 
 const ContentWrap = styled.div`
@@ -134,23 +96,7 @@ const SeeMore = styled.div`
 
 const TopBlock = () => (
   <Body>
-    <Header>
-      <LogoWrapper>
-        <img src={logoBlue} />
-      </LogoWrapper>
-      <Menu>
-        <MenuItem>
-          <a href="https://github.com/ms314006/biskett-clone">
-            OOOO
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a href="https://github.com/ms314006/biskett-clone">
-            XXXX
-          </a>
-        </MenuItem>
-      </Menu>
-    </Header>
+    <Header />
     <ContentWrap>
       <Introduction superQ1={superQ1}>
         <h1>OOOOOO</h1>
